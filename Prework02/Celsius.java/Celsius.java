c8c23c0064ef5ceb0fed5689fae25304c38dddeb
Halseyjhs <96216781+pelfers21@users.noreply.github.com>
2022-02-14 01:58:56 +0000

    Edited Celsius to allow for decimals

--- Prework02/Celsius.java/Celsius.java
+++ Prework02/Celsius.java/Celsius.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
 
-/**
- * Converts Celcius to Fahrenheight.
- */
 public class Celsius {
+	public static void main(String[] args) {
+		// Create new Scanner object
+		Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int cel;
-        double far;
-        Scanner in = new Scanner(System.in);
-      //asks for input from the user
-        System.out.print("What is the temperature in Celsius? ");
-        cel = in.nextInt();
-      //creates a constant for 9/5 or 1.8
-        final double CEL_TO_FAR = 1.8;
-      
-       //formula for conversion
-        far = cel * CEL_TO_FAR + 32;
-        System.out.print(cel + " Celsius = ");
-        //prints out temperature in Fahrenheight
-        System.out.println(far + " Fahrenheight");
-    }
+		// Prompt user to input a double
+		System.out.print("Enter a degree in Celsius: ");
+		double celsius = input.nextDouble();
 
+		// Convert Celsius to Fahrenheit
+		double fahrenheit = 9.0 / 5 * celsius + 32;
+
+		// Display results
+		System.out.println(celsius + " Celsius is " + fahrenheit
+			+ " Fahrenheit"); 
+	}
 }
